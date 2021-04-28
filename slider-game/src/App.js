bc5c94dad4f88314da8fcc95b54feafc2550b1f8
soumavm <soumavm@gmail.com>
2021-04-27 18:55:35 -0600

    implemented React app

---
+++ slider-game/src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react"
+import LinkedList from "./LinkedList"
+
+
+class App extends Component{
+  constructor(){
+    super()
+}
+
+  render(){
+    return(
+      <div>
+        <LinkedList />
+      </div>
+    )
+  }
+}
+
+export default App;
